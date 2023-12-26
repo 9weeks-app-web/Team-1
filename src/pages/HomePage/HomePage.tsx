979efc704b9cf8f4d2c793da79447e5fd57b1cd7
wjs_5025 <wjs_5025@naver.com>
2023-12-27 01:35:07 +0900

    Fix: eslint 설정에 따른 코드포맷 변경

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,16 +1,12 @@
-import React from 'react';
-import fireStore from 'api/firestoreConfig';
-import "./style.css"
+import React from "react";
+import fireStore from "api/firestoreConfig";
+import "./style.css";
 
 function HomePage() {
-	// firebase connect test
-	console.log(fireStore)
-	
-	return (
-		<div className='container'>
-			홈
-		</div>
-	);
+  // firebase connect test
+  console.log(fireStore);
+
+  return <div className="container">홈</div>;
 }
 
 export default HomePage;
