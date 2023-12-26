e86f772b946dac2f3965f8557c043d84254a3ab3
wjs5025 <wjs_5025@naver.com>
2023-12-26 23:57:08 +0900

    Rename : 페이지 컴포넌트명 변경

---
+++ src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import fireStore from 'api/firestoreConfig';
+import "./style.css"
+
+function HomePage() {
+	// firebase connect test
+	console.log(fireStore)
+	
+	return (
+		<div className='container'>
+			홈
+		</div>
+	);
+}
+
+export default HomePage;
