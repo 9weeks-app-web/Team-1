cc7148cef031b6964e4e48afc2820ed02c01f1e9
99narii <122865149+99narii@users.noreply.github.com>
2024-01-02 18:27:21 +0900

    modify

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import AppRouter from 'router/AppRouter';
-import "../src/style/GlobalStyles.css";
+import React from "react";
+import AppRouter from "router/AppRouter";
+import "./style/GlobalStyles.css";
 
 function App() {
-	return <AppRouter />;
+  return <AppRouter />;
 }
 
 export default App;
--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,28 +1,22 @@
 import React from "react";
 import HomePage from "pages/HomePage/HomePage";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-<<<<<<< Updated upstream
-=======
 import Login from "pages/Auth/Login";
 import Register from "pages/Auth/Register";
 import MyPage from "pages/MyPage";
 import Header from "components/Header";
 import Chat from "pages/Chat";
->>>>>>> Stashed changes
 
 function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
-<<<<<<< Updated upstream
-=======
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/mypage" element={<MyPage />} />
         <Route path="/portfolio" element={<PortfolioPage />} />
         <Route path="/chat" element={<Chat />} />
->>>>>>> Stashed changes
       </Routes>
     </BrowserRouter>
   );
