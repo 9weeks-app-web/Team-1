3b588fe9e29d4e4eb5a77d1e3d19d9e14e590848
wjs_5025 <wjs_5025@naver.com>
2024-01-10 06:07:50 +0900

    modify : 멘토카드

--- src/components/Cards/MentorRowCard/MentorRowCard.tsx
+++ src/components/Cards/MentorRowCard/MentorRowCard.tsx
@@ -3,13 +3,17 @@ import "./style.css";
 import { IMember } from "types/mentor";
 import MentoringBadge from "components/Badges/MentoringBadge/MentoringBadge";
 import { useNavigate } from "react-router-dom";
+import Icon from "components/Icon/Icon";
+import Badge from "components/Badges/Badge/Badge";
 
 interface IMentorRowCard {
   member: IMember;
+  isBest?: boolean;
+  isNew?: boolean;
 }
 
 function MentorRowCard(props: IMentorRowCard) {
-  const { member } = props;
+  const { member, isBest, isNew } = props;
   const navigate = useNavigate();
 
   return (
@@ -24,19 +28,34 @@ function MentorRowCard(props: IMentorRowCard) {
       <img className="mentor-profileImg" src={member.profileImg} alt="" />
       <div className="member-info">
         <div>
-          <p className="member-name">{member.name}</p>
-          <p className="member-rating">{member.rating}</p>
+          <p className="member-name">
+            <Icon icon="trophy" className="best-icon" />
+            {isBest && <Icon icon="trophy" />}
+            {isNew && <span>NEW</span>}
+            {member.name}
+          </p>
+          <p className="member-rating">
+            <Icon icon="star" className="star-icon" />
+            {member.rating}
+          </p>
         </div>
-        <div className="sub-info oneLineEllipsis">
-          직무 <span>{member.occupation}</span>
-        </div>
-        <div className="sub-info oneLineEllipsis">
-          <span>경력</span>
-          <span>{member.career}</span>
+        <div className="flex-column gap-8">
+          <div className="sub-info oneLineEllipsis">
+            직무 <span>{member.occupation}</span>
+          </div>
+          <div className="sub-info oneLineEllipsis">
+            경력
+            <span>{member.career}</span>
+          </div>
         </div>
       </div>
       <div className="member-introduction">
-        <div>시간 엄수</div>
+        <div className="member-badges">
+          <Badge text="시간엄수" type="teal" />
+          <Badge text="꼼꼼한" type="teal" />
+          <Badge text="이성적인" type="teal" />
+          <Badge text="+3" type="teal" />
+        </div>
         <div>{member.introduction}</div>
       </div>
     </div>
